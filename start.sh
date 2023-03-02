0a24045f681c5e2a65c4a29ebaea810283b192c2
sangje <parksj2480@naver.com>
2023-03-02 20:45:55 +0900

    checkpoint eliminated

--- start.sh
+++ start.sh
@@ -44,7 +44,6 @@ python3 main.py \
         --weight_decay 0.0000001 \
         --input_layer $input_layer \
         --pos_enc_layer_type $pos_enc_layer_type \
-        --checkpoint_path $checkpoint_path \
         --pairs \
         --aug
 
